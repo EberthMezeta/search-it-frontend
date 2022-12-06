2288be4d43962b8cba6bb59cdd3d47dfdce6fea0
Eberth Mezeta <the.jonkey@gmail.com>
2022-12-06 02:41:24 -0600

    Creacion varios servicios

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,47 @@
+import { EmailService } from './../../services/email/email.service';
+import { StatisticsService } from './../../services/statistics/statistics.service';
+import { Statistics } from './../../interfaces/statistics';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DocumentsService } from 'src/app/services/documents/documents.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  public fileUrl !: any;
 
-  constructor() { }
+  public statistics !: Statistics;
+
+  constructor(private documentService: DocumentsService ,private statisticService : StatisticsService, private emailService : EmailService) {}
 
   ngOnInit(): void {
+    this.statisticService.getStatistics().subscribe(data => {
+      this.statistics = data;
+      console.log(this.statistics);
+    });
   }
 
+  public download() {
+    this.documentService.downloadDocument('Mirame.pdf').subscribe((response) => {
+     console.log(response);
+     //let filename = response.headers.get('Content-Disposition')?.split(';')[1].split('=')[1];
+     let filename = 'Mirame.pdf';
+      let blob: Blob = response;
+      let a = document.createElement('a');
+      a.download = filename;
+      a.href = window.URL.createObjectURL(blob);
+      a.click();
+
+    });
+    console.log('Descargando');
+  }
+
+  public sendEmail() {
+    this.emailService.sendEmail("the.jonkey@gmail.com","http://artrune.com:8093/download/Mirame.pdf").subscribe(data => {
+      console.log(data);
+    });
+  }
 }
--- src/app/services/documents/documents.service.ts
+++ src/app/services/documents/documents.service.ts
@@ -1,3 +1,6 @@
+import { CookieService } from 'ngx-cookie-service';
+import { HttpHeaders } from '@angular/common/http';
+import { ResponseUpload } from './../../interfaces/response-upload';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -11,8 +14,26 @@ export class DocumentsService {
   private URL_DOCUMENT_UPLOAD_SERVICE = 'http://artrune.com:8093/file/';
   private URL_DOCUMENT_DOWNLOAD_SERVICE = 'http://artrune.com:8093/download/';
 
-  constructor(private httpClient : HttpClient) {  }
+  constructor(private httpClient : HttpClient, private cookieService:CookieService) {  }
 
+  public uploadDocument(file: File): Observable<ResponseUpload> {
+    const headers = this.getHeaders();
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.httpClient.post<ResponseUpload>(this.URL_DOCUMENT_UPLOAD_SERVICE, formData, {headers});
+  }
+
+  public downloadDocument(filename: string){
+    let Bearer = this.cookieService.get('token');
+    let headers = new HttpHeaders({'Authorization': 'Bearer ' + Bearer, 'accept': 'application/json'});
+    return this.httpClient.get(this.URL_DOCUMENT_DOWNLOAD_SERVICE + filename,{headers, responseType: 'blob'});
+  }
+
+  private getHeaders(): HttpHeaders {
+    let Bearer = this.cookieService.get('token');
+    let headers = new HttpHeaders({'Authorization': 'Bearer ' + Bearer});
+    return headers;
+  }
 
 
 
